2cd846ca6112bf3380880dcf94d848b64c31f753
Morgan <hifrommorgan@icloud.com>
2016-11-19 08:20:40 -0600

    added reverseWords & removePronouns
    the methods are self-descriptive

--- mission/lib/transforms.js
+++ mission/lib/transforms.js
@@ -1,4 +1,19 @@
 var TRANSFORMERS = {
+	var pronouns = ["I", "me", "we", "us", "you", "she"
+									"her", "he", "him", "it", "they", "them",
+									"that", "which", "who", "whom", "whichever",
+									"whoever", "whomever", "this", "these",
+									"that", "those", "anybody", "anyone", "anything",
+									"each", "either", "everybody", "everyone",
+									"everything", "neither", "nobody", "no one",
+									"somebody", "someone", "something", "few", "many",
+									"several", "all", "any", "most", "none", "some",
+									"myself", "ourselves", "yourself", "yourselves",
+									"himself", "herself", "itself", "themselves",
+									"my", "your", "his", "her", "its", "our", "their",
+									"mine", "yours", "hers", "ours", "theirs"];
+
+// not exhaustive, certain words removed just in case of usability
 
 	normal: function(msg){
 		return msg;
@@ -16,4 +31,20 @@ var TRANSFORMERS = {
 		return nMsg;
 	}
 
+	reverseWords: function(msg) {
+		msg.split("").reverse().join("").split(" ").reverse().join(" ");
+		return msg;
+	}
+
+	removePronouns: function(msg) {
+		var words = msg.split(" ");
+		for (var i = 0; i < words.length; i++) {
+			for (var j = 0; j < pronouns.length; j++) {
+				if (words[i] == pronouns[j]) {
+					words[i] == "";
+				}
+			}
+		}
+		return words.join(" ");
+	}
 }
