a246b3a8738c30106ce6ab88da58d09577a06856
Haosheng Li <hli109@hawk.iit.edu>
2016-11-19 14:11:42 -0600

    video casting

--- graphics/input_test.js
+++ graphics/input_test.js
@@ -8,22 +8,31 @@ b4w.register("input_test", function(exports, require) {
 var m_anim      = require("animation");
 var m_app       = require("app");
 var m_data      = require("data");
+var m_cfg 		= require("config");
+var m_version	= require("version");
+var m_textures  = require("textures");
 var m_scenes    = require("scenes");
 var m_material  = require("material");
+var m_cont 		= require("container");
+
 var m_util 		= require("util");
 var m_lights 	= require("lights");
-
 var m_geometry  = require("geometry");
 var m_transform = require("transform");
 
+// variables
+var TIME_DELAY = 1000 / 24;
+var WAITING_DELAY = 1000;
+var DEBUG = (m_version.type() === "DEBUG");
 var _previous_selected_obj = null;
+var _cam_waiting_handle = null;
 
 /**
  * export the method to initialize the app (called at the bottom of this file)
  */
 exports.init = function() {
     m_app.init({
-        canvas_container_id: "main_canvas_container",
+        canvas_container_id: "main_canvas_container", // name of id for the canvas in index.html
         callback: init_cb,
         show_fps: true,
         console_verbose: true,
@@ -87,11 +96,68 @@ function load_cb(data_id) {
     m_app.enable_camera_controls();
 
     // place your code here
-    m_anim.play(m_scenes.get_object_by_name("Icosphere0"));
-    m_anim.play(m_scenes.get_object_by_name("Icosphere2"));
-    m_anim.play(m_scenes.get_object_by_name("Icosphere4"));
+    // m_anim.play(m_scenes.get_object_by_name("Icosphere0"));
+    // m_anim.play(m_scenes.get_object_by_name("Icosphere2"));
+    // m_anim.play(m_scenes.get_object_by_name("Icosphere4"));
+
+    //var error_cap = m_scenes.get_object_by_name("Text");
+    m_app.enable_camera_controls();
+
+    if (Boolean(get_user_media()))
+        start_video();
 }
 
+function get_user_media() {
+    if (Boolean(navigator.getUserMedia))
+        return navigator.getUserMedia.bind(navigator);
+    else if (Boolean(navigator.webkitGetUserMedia))
+        return navigator.webkitGetUserMedia.bind(navigator);
+    else if (Boolean(navigator.mozGetUserMedia))
+        return navigator.mozGetUserMedia.bind(navigator);
+    else if (Boolean(navigator.msGetUserMedia))
+        return navigator.msGetUserMedia.bind(navigator);
+    else
+        return null;
+}
+
+function start_video() {
+
+    if (_cam_waiting_handle)
+        clearTimeout(_cam_waiting_handle);
+
+    var user_media = get_user_media();
+    var media_stream_constraint = {
+        video: { width: 1280, height: 720 }
+    };
+    var success_cb = function(local_media_stream) {
+        var video = document.createElement("video");
+        video.setAttribute("autoplay", "true");
+        video.src = window.URL.createObjectURL(local_media_stream);
+        // var error_cap = m_scenes.get_object_by_name("Text");
+        // m_scenes.hide_object(error_cap);
+
+        var obj = m_scenes.get_object_by_name("TV_R"); // name of the object 
+        var context = m_textures.get_canvas_ctx(obj, "Texture.001");
+        var update_canvas = function() {
+        	//context.change_image(obj, "Texture.001", "blendFiles/_texture/screen.png");
+        	//context.play_video(video);
+            context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, context.canvas.width, context.canvas.height);
+            m_textures.update_canvas_ctx(obj, "Texture.001");
+            setTimeout(function() {update_canvas()}, TIME_DELAY);
+        }
+
+        video.onloadedmetadata = function(e) {
+            update_canvas();
+        };
+    };
+
+    var fail_cb = function() {
+        //var error_cap = m_scenes.get_object_by_name("Text");
+        _cam_waiting_handle = setTimeout(start_video, WAITING_DELAY);
+    };
+
+    user_media(media_stream_constraint, success_cb, fail_cb);
+}
 
 });
 
