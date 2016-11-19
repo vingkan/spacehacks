9e755e963efc4afd1ba53b79060eec97f65ea04c
Vinesh Kannan <vkannan3@hawk.iit.edu>
2016-11-19 15:00:10 -0600

    Simple data uri projection

--- graphics/input_test.js
+++ graphics/input_test.js
@@ -141,12 +141,23 @@ function start_video() {
         var update_canvas = function() {
         	//context.change_image(obj, "Texture.001", "blendFiles/_texture/screen.png");
         	//context.play_video(video);
-            context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, context.canvas.width, context.canvas.height);
+        var dataURL = 'data:image/gif;base64,R0lGODdhMAAwAPAAAAAAAP///ywAAAAAMAAw AAAC8IyPqcvt3wCcDkiLc7C0qwyGHhSWpjQu5yqmCYsapyuvUUlvONmOZtfzgFz ByTB10QgxOR0TqBQejhRNzOfkVJ+5YiUqrXF5Y5lKh/DeuNcP5yLWGsEbtLiOSp a/TPg7JpJHxyendzWTBfX0cxOnKPjgBzi4diinWGdkF8kjdfnycQZXZeYGejmJl ZeGl9i2icVqaNVailT6F5iJ90m6mvuTS4OK05M0vDk0Q4XUtwvKOzrcd3iq9uis F81M1OIcR7lEewwcLp7tuNNkM3uNna3F2JQFo97Vriy/Xl4/f1cf5VWzXyym7PH hhx4dbgYKAAA7';
+
+        var imgObj = new Image();
+        imgObj.crossOrigin = 'anonymous';
+        imgObj.src = dataURL;
+        imgObj.onload = function(){
+            //ctx.drawImage(this, 0, 0);
+            //ctx.drawImage(imgObj, 0, 0, 200, 200, 0, 0, ctx.canvas.width, ctx.canvas.height);
+            context.drawImage(imgObj, 0, 0, context.canvas.width, context.canvas.height);
+            console.log(context.canvas.width, context.canvas.height)
+        }
+        
+
+            //context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, context.canvas.width, context.canvas.height);
             m_textures.update_canvas_ctx(obj, "Texture.001");
             setTimeout(function() {update_canvas()}, TIME_DELAY);
         }
-
-        console.log('Vinesh was here.');
 
         video.onloadedmetadata = function(e) {
             update_canvas();
