77fc31be795410f6643b8f716f5aa68ac0a85bce
Vinesh Kannan <vkannan3@hawk.iit.edu>
2016-11-20 04:03:16 -0600

    Barebones companion controller

--- graphics/input_test.js
+++ graphics/input_test.js
@@ -1,6 +1,30 @@
 "use strict"
 
-var ROOM_KEY = false;
+var QueryString = function () {
+  // This function is anonymous, is executed immediately and 
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  } 
+  return query_string;
+}();
+
+//var ROOM_KEY = window.opener.ROOM_KEY || false;
+var ROOM_KEY = QueryString.gid || false;
 
 // var THREE = require('three');
 // var StereoEffect = require('three-stereo-effect')(THREE);
@@ -199,10 +223,10 @@ var init_screen = function() {
 
 });
 
-var promise = db.ref('games').push({timestamp: Date.now()});
-promise.then(function(data){
-    window.ROOM_KEY = promise.path['o'][1];
+window.initGame = function(roomKey){
+    window.ROOM_KEY = roomKey;
     // import the app module and start the app by calling the init method
     b4w.require("stereo_view").init();
-});
+}
 
+initGame(window.ROOM_KEY);
