991b3a569fa4a782cbd9dc23d0afbaaa63cd5fda
Vinesh Kannan <vkannan3@hawk.iit.edu>
2016-11-20 02:06:25 -0600

    Updated stereo page with circuit module

--- graphics/stereo_view.js
+++ graphics/stereo_view.js
@@ -1,5 +1,7 @@
 "use strict"
 
+var ROOM_KEY = false;
+
 // var THREE = require('three');
 // var StereoEffect = require('three-stereo-effect')(THREE);
 // register the application module
@@ -48,6 +50,45 @@ var _cam_waiting_handle = null;
 var _dest_x_trans = 0;
 var _dest_z_trans = 0;
 
+MissionLink.sync(window.ROOM_KEY);
+
+
+/**
+ * Support sketchy iFrame operations by donating to W3 today!
+ */
+function getIframeWindow(iframe_object) {
+  var doc;
+
+  if (iframe_object.contentWindow) {
+    return iframe_object.contentWindow;
+  }
+
+  if (iframe_object.window) {
+    return iframe_object.window;
+  } 
+
+  if (!doc && iframe_object.contentDocument) {
+    doc = iframe_object.contentDocument;
+  } 
+
+  if (!doc && iframe_object.document) {
+    doc = iframe_object.document;
+  }
+
+  if (doc && doc.defaultView) {
+   return doc.defaultView;
+  }
+
+  if (doc && doc.parentWindow) {
+    return doc.parentWindow;
+  }
+
+  return undefined;
+}
+
+/*var el = document.getElementById('targetFrame');
+getIframeWindow(el).putme();*/
+
 /**
  * export the method to initialize the app (called at the bottom of this file)
  */
@@ -83,6 +124,13 @@ function preloader_cb(percentage) {
     m_preloader.update_preloader(percentage);
 }
 
+var BUTTON_MAPPING = {
+    'Circle': 'alpha',
+    'Square': 'beta',
+    'Triangle': 'gamma',
+    'Diamond': 'delta'
+}
+
 function main_canvas_click(e) {
     if (e.preventDefault)
         e.preventDefault();
@@ -107,6 +155,7 @@ function main_canvas_click(e) {
             m_anim.apply_def(obj);
             m_anim.play(obj);
             console.log(obj);
+            MissionLink._pushButton(BUTTON_MAPPING[obj.name]);
         }
     }
 }
@@ -128,8 +177,7 @@ function load_cb(data_id) {
 
     m_gyro.enable_camera_rotation();
 
-    if (Boolean(get_user_media()))
-        start_video();
+    init_screen();
 
     console.log(m_hmd.check_browser_support());
     if (m_hmd.check_browser_support())
@@ -193,54 +241,32 @@ function register_mouse(is_hmd) {
     //         m_ctl.CT_POSITIVE, [time], null, shoot_cb);
 }
 
-function get_user_media() {
-    if (Boolean(navigator.getUserMedia))
-        return navigator.getUserMedia.bind(navigator);
-    else if (Boolean(navigator.webkitGetUserMedia))
-        return navigator.webkitGetUserMedia.bind(navigator);
-    else if (Boolean(navigator.mozGetUserMedia))
-        return navigator.mozGetUserMedia.bind(navigator);
-    else if (Boolean(navigator.msGetUserMedia))
-        return navigator.msGetUserMedia.bind(navigator);
-    else
-        return null;
-}
+var init_screen = function() {
+    // var error_cap = m_scenes.get_object_by_name("Text");
+    // m_scenes.hide_object(error_cap);
 
+    var circuitWindow = document.getElementById('circuits');
+    getIframeWindow(circuitWindow).init(MissionLink.getRoomKey());
 
-function start_video() {
+    var obj = m_scenes.get_object_by_name("TV_R");
+    var context = m_textures.get_canvas_ctx(obj, "Texture.001");
+    var update_canvas = function() {
+        db.ref(MissionLink.getRoomKey() + '/modules/circuits/data-uri').once('value', function(snap){
+            var dataURI = snap.val();
+            if(dataURI){
+                var imgObj = new Image();
+                imgObj.crossOrigin = 'anonymous';
+                imgObj.src = dataURI;
+                imgObj.onload = function(){
+                    context.drawImage(imgObj, 0, 0, context.canvas.width, context.canvas.height);
+                }
+            }
+        });
+        m_textures.update_canvas_ctx(obj, "Texture.001");
+        setTimeout(function() {update_canvas()}, TIME_DELAY);
+    }
 
-    if (_cam_waiting_handle)
-        clearTimeout(_cam_waiting_handle);
-
-    var user_media = get_user_media();
-    var media_stream_constraint = {
-        video: { width: 1280, height: 720 }
-    };
-    var success_cb = function(local_media_stream) {
-        var video = document.createElement("video");
-        video.setAttribute("autoplay", "true");
-        video.src = window.URL.createObjectURL(local_media_stream);
-
-        var obj = m_scenes.get_object_by_name("TV_R"); // name of the object 
-        var context = m_textures.get_canvas_ctx(obj, "Texture.001");
-        var update_canvas = function() {
-        	//context.change_image(obj, "Texture.001", "blendFiles/_texture/screen.png");
-        	//context.play_video(video);
-            context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, context.canvas.width, context.canvas.height);
-            m_textures.update_canvas_ctx(obj, "Texture.001");
-            setTimeout(function() {update_canvas()}, TIME_DELAY);
-        }
-
-        video.onloadedmetadata = function(e) {
-            update_canvas();
-        };
-    };
-
-    var fail_cb = function() {
-        _cam_waiting_handle = setTimeout(start_video, WAITING_DELAY);
-    };
-
-    user_media(media_stream_constraint, success_cb, fail_cb);
+    update_canvas();
 }
 
 //==============================================================================
@@ -282,6 +308,9 @@ function register_hmd() {
 
 });
 
-// import the app module and start the app by calling the init method
-b4w.require("stereo_view").init();
-
+var promise = db.ref('games').push({timestamp: Date.now()});
+promise.then(function(data){
+    window.ROOM_KEY = promise.path['o'][1];
+    // import the app module and start the app by calling the init method
+    b4w.require("stereo_view").init();
+});
