8d097e7abe2b9800b9ae2a9f62ee0183b0075321
Vinesh Kannan <vkannan3@hawk.iit.edu>
2016-11-19 18:41:18 -0600

    Fix lost progress

--- graphics/input_test.js
+++ graphics/input_test.js
@@ -1,24 +1,25 @@
 "use strict"
 
+// var THREE = require('three');
+// var StereoEffect = require('three-stereo-effect')(THREE);
 // register the application module
-b4w.register("input_test", function(exports, require) {
+b4w.register("stereo_view", function(exports, require) {
 
-console.log('difference')
 
 // import modules used by the app
 var m_anim      = require("animation");
 var m_app       = require("app");
 var m_data      = require("data");
-var m_cfg 		= require("config");
-var m_version	= require("version");
+var m_cfg       = require("config");
+var m_version   = require("version");
 var m_textures  = require("textures");
 var m_scenes    = require("scenes");
 var m_material  = require("material");
-var m_cont 		= require("container");
+var m_cont      = require("container");
 var m_gyro      = require("gyroscope");
 
-var m_util 		= require("util");
-var m_lights 	= require("lights");
+var m_util      = require("util");
+var m_lights    = require("lights");
 var m_geometry  = require("geometry");
 var m_transform = require("transform");
 
@@ -76,7 +77,8 @@ exports.init = function() {
         callback: init_cb,
         show_fps: true,
         console_verbose: true,
-        autoresize: true
+        autoresize: true,
+        stereo: "HMD"
     });
 }
 
@@ -118,12 +120,11 @@ function main_canvas_click(e) {
         }
         _previous_selected_obj = obj;
 
-        if (obj.name == "fullscreen")
-            toggleFullScreen();
-
         m_anim.apply_def(obj);
         m_anim.play(obj);
-
+        // m_anim.play(obj, function(data) {
+        //  m_anim.stop(data);
+        // });
         console.log(obj);
         MissionLink._pushButton(BUTTON_MAPPING[obj.name]);
     }
@@ -133,7 +134,7 @@ function main_canvas_click(e) {
  * load the scene data
  */
 function load() {
-	// name of the json file exported from blender
+    // name of the json file exported from blender
     m_data.load("Engine.json", load_cb);
 }
 
@@ -149,9 +150,24 @@ function load_cb(data_id) {
     if (Boolean(get_user_media()))
         start_video();
 
-    // prints out the canvas
     var stereoCanvas = m_textures.get_canvas_ctx(m_scenes.get_object_by_name("TV_R"), "Texture.001");
-    console.log(stereoCanvas.canvas);
+    console.log(stereoCanvas);
+
+}
+
+function toggleFullScreen() {
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    requestFullScreen.call(docEl);
+  }
+  else {
+    cancelFullScreen.call(doc);
+  }
 }
 
 function get_user_media() {
@@ -167,6 +183,7 @@ function get_user_media() {
         return null;
 }
 
+
 function start_video() {
 
     if (_cam_waiting_handle)
@@ -177,58 +194,29 @@ function start_video() {
         video: { width: 1280, height: 720 }
     };
     var success_cb = function(local_media_stream) {
-        var video = document.createElement("video");
-        video.setAttribute("autoplay", "true");
-        video.src = window.URL.createObjectURL(local_media_stream);
         // var error_cap = m_scenes.get_object_by_name("Text");
         // m_scenes.hide_object(error_cap);
 
-        var obj = m_scenes.get_object_by_name("TV_R"); // name of the object 
+        var obj = m_scenes.get_object_by_name("TV_R");
         var context = m_textures.get_canvas_ctx(obj, "Texture.001");
         var update_canvas = function() {
-        	//context.change_image(obj, "Texture.001", "blendFiles/_texture/screen.png");
-        	//context.play_video(video);
-
-        db.ref('modules/circuits/data-uri').on('value', function(snap){
-
-        });
-        var dataURI = 'data:image/gif;base64,R0lGODdhMAAwAPAAAAAAAP///ywAAAAAMAAw AAAC8IyPqcvt3wCcDkiLc7C0qwyGHhSWpjQu5yqmCYsapyuvUUlvONmOZtfzgFz ByTB10QgxOR0TqBQejhRNzOfkVJ+5YiUqrXF5Y5lKh/DeuNcP5yLWGsEbtLiOSp a/TPg7JpJHxyendzWTBfX0cxOnKPjgBzi4diinWGdkF8kjdfnycQZXZeYGejmJl ZeGl9i2icVqaNVailT6F5iJ90m6mvuTS4OK05M0vDk0Q4XUtwvKOzrcd3iq9uis F81M1OIcR7lEewwcLp7tuNNkM3uNna3F2JQFo97Vriy/Xl4/f1cf5VWzXyym7PH hhx4dbgYKAAA7';
-
-        var imgObj = new Image();
-        imgObj.crossOrigin = 'anonymous';
-        imgObj.src = dataURI;
-        imgObj.onload = function(){
-            //ctx.drawImage(this, 0, 0);
-            //ctx.drawImage(imgObj, 0, 0, 200, 200, 0, 0, ctx.canvas.width, ctx.canvas.height);
-            context.drawImage(imgObj, 0, 0, context.canvas.width, context.canvas.height);
-            console.log(context.canvas.width, context.canvas.height)
-        }
-        
-
-            //context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, context.canvas.width, context.canvas.height);
+            db.ref('modules/circuits/data-uri').once('value', function(snap){
+                var dataURI = snap.val();
+                if(dataURI){
+                    var imgObj = new Image();
+                    imgObj.crossOrigin = 'anonymous';
+                    imgObj.src = dataURI;
+                    imgObj.onload = function(){
+                        context.drawImage(imgObj, 0, 0, context.canvas.width, context.canvas.height);
+                    }
+                }
+            });
             m_textures.update_canvas_ctx(obj, "Texture.001");
             setTimeout(function() {update_canvas()}, TIME_DELAY);
         }
 
-        video.onloadedmetadata = function(e) {
-            update_canvas();
-        };
+        update_canvas();
     };
-
-    function toggleFullScreen() {
-  		var doc = window.document;
-  		var docEl = doc.documentElement;
-	
-  		var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-  		var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-
-  		if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-    		requestFullScreen.call(docEl);
-  		}
-  		else {
-    		cancelFullScreen.call(doc);
-  		}
-	}
 
     var fail_cb = function() {
         //var error_cap = m_scenes.get_object_by_name("Text");
@@ -241,5 +229,5 @@ function start_video() {
 });
 
 // import the app module and start the app by calling the init method
-b4w.require("input_test").init();
+b4w.require("stereo_view").init();
 
